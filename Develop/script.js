fc0eecfa2f99489d84b2dfcb10442caa0db01068
GMMarshall <gmmarshall27@gmail.com>
2021-09-16 21:34:54 -0400

    switched my confirm functions into the else if statement and moved the alert to else so to all prombts would work correctly. also added in another else if statment to confirm between the numbers. also moved all variable inside of if statemnts

--- Develop/script.js
+++ Develop/script.js
@@ -9,31 +9,45 @@ function writePassword() {
   passwordText.value = password;
 
   function generatePassword() {
- //Define numbers
-    var numbers =[0,1,2,3,4,5,6,7,8,9];
-// Define lower case letters
-    var lowerCase = ["a", "b","c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m","n", "o", "p", "q", "r", "s", "t", "u", "v","w","x","y","z"]
-//Define upper case letters
-    var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M" , "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-//Define special characters
-    var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*","’", "(", ")", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "~", "{","}","[","]"]
- //created prompt to chose password length   
+
+    var allCharacters = []
+    //created prompt to chose password length   
     passwordLength = prompt("How long do you want your password? Please choose between 8 and 128");
     if (!passwordLength){
-      alert("Please select a value!");
+      alert("Sorry you must input a number!");
     } else if (passwordLength < 8 || passwordLength > 128){
-      passwordLength = prompt("Please choose a number between 8 and 128!");
-    } else {
+      passwordLength = prompt("You must choose a number between 8 and 128!");
+    }
+    else if (passwordLength >7 && passwordLength <129){
       numbers = confirm("Will the password include numbers?");
       lowerCase = confirm("Will the password include lower case letters?");
       upperCase = confirm("Will the password include upper case letters?");
       specialCharacters = confirm("will the password include special characters?");
+    } else {
+      alert("You must pick a number!")
+      console.log(numbers);
     };
     //added if statement so that if nothing is selected alert is sent
     if (!numbers && !lowerCase && !upperCase && !specialCharacters ){
-        alert("Must select at least 1 option.");
-    };
-
+        alert("Sorry, you must select at least 1 option.");
+    }
+    if (numbers){
+      var numbers =["0","1","2","3","4","5","6","7","8","9"];
+      allCharacters=[...numbers,...allCharacters]; 
+    }
+    if (lowerCase){
+      var lowerCase = ["a", "b","c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m","n", "o", "p", "q", "r", "s", "t", "u", "v","w","x","y","z"];
+      allCharacters=[...lowerCase,...allCharacters];
+    }
+    if (upperCase){
+      var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M" , "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+      allCharacters=[...upperCase,...allCharacters];
+    }
+    if (specialCharacters){
+      var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*","’", "(", ")", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "~", "{","}","[","]"];
+      allCharacters=[...specialCharacters,...allCharacters];
+    }
+   
   }
 
 }
