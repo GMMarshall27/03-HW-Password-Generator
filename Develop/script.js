7f425f3bb536c2a8c4a1105c104cf6b526689c06
GMMarshall <gmmarshall27@gmail.com>
2021-09-16 22:07:23 -0400

    added comments to js file

--- Develop/script.js
+++ Develop/script.js
@@ -9,8 +9,8 @@ function writePassword() {
   passwordText.value = password;
 
   function generatePassword() {
-
-    var allCharacters = []
+    //variable 
+    var allCharacters = [];
     //created prompt to chose password length   
     passwordLength = prompt("How long do you want your password? Please choose between 8 and 128");
     if (!passwordLength){
@@ -31,23 +31,35 @@ function writePassword() {
     if (!numbers && !lowerCase && !upperCase && !specialCharacters ){
         alert("Sorry, you must select at least 1 option.");
     }
+     //moved number varialbe into if statment and used spread syntax
     if (numbers){
       var numbers =["0","1","2","3","4","5","6","7","8","9"];
       allCharacters=[...numbers,...allCharacters]; 
     }
+    //moved lowerCase varialbe into if statment and used spread syntax
     if (lowerCase){
       var lowerCase = ["a", "b","c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m","n", "o", "p", "q", "r", "s", "t", "u", "v","w","x","y","z"];
       allCharacters=[...lowerCase,...allCharacters];
     }
+    //moved upperCase varialbe into if statment and used spread syntax
     if (upperCase){
       var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M" , "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
       allCharacters=[...upperCase,...allCharacters];
     }
+    //moved specialCharacters varialbe into if statment and used spread syntax
     if (specialCharacters){
       var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*","’", "(", ")", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "~", "{","}","[","]"];
       allCharacters=[...specialCharacters,...allCharacters];
     }
-   
+    for(i=0; i<passwordLength; i++) {
+      var index = Math.floor(Math.random() * allCharacters.length);
+       password += allCharacters[index];
+    }
+    
+console.log(allCharacters);
+
+    return password;
+    
   }
 
 }
